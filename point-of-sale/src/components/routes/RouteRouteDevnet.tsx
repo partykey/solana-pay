8b2271d56f58df713a7854d50e437bfd42b6bf39
partykey <partykey@gmail.com>
2022-02-16 22:06:35 +0100

    Create RouteRouteDevnet.tsx

---
+++ point-of-sale/src/components/routes/RouteRouteDevnet.tsx
@@ -0,0 +1,98 @@
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { TorusWalletAdapter } from '@solana/wallet-adapter-torus';
+import { PublicKey } from '@solana/web3.js';
+import React, { FC, useMemo } from 'react';
+import { Outlet, useSearchParams } from 'react-router-dom';
+import { DEVNET_ENDPOINT } from '../../utils/constants';
+import { ConfigProvider } from '../contexts/ConfigProvider';
+import { FullscreenProvider } from '../contexts/FullscreenProvider';
+import { PaymentProvider } from '../contexts/PaymentProvider';
+import { ThemeProvider } from '../contexts/ThemeProvider';
+import { TransactionsProvider } from '../contexts/TransactionsProvider';
+import { SolanaPayLogo } from '../images/SolanaPayLogo';
+import { SOLIcon } from '../images/SOLIcon';
+import * as css from './RootRoute.module.pcss';
+//updated 2/14/22
+//import the following 2 lines to accept USDC on Mainnet
+//import { MAINNET_ENDPOINT, MAINNET_USDC_MINT } from '../../utils/constants';
+//import { USDCIcon } from '../images/USDCIcon';
+
+export const RootRoute: FC = () => {
+    // If you're testing without a phone, set this to true to allow a browser-based wallet connection to be used
+    // Updated 2/14/22
+    const connectWallet = true;
+    const wallets = useMemo(
+        () => (connectWallet ? [new PhantomWalletAdapter(), new TorusWalletAdapter()] : []),
+        [connectWallet]
+    );
+
+    const [params] = useSearchParams();
+    const { recipient, label } = useMemo(() => {
+        let recipient: PublicKey | undefined, label: string | undefined;
+
+        const recipientParam = params.get('recipient');
+        const labelParam = params.get('label');
+        if (recipientParam && labelParam) {
+            try {
+                recipient = new PublicKey(recipientParam);
+                label = labelParam;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        return { recipient, label };
+    }, [params]);
+
+    //devnet original values below
+    //<ConnectionProvider endpoint={DEVNET_ENDPOINT}>
+    //symbol="SOL"
+    //icon={<SOLIcon />}
+    //decimals={9}
+    //minDecimals={1}
+
+    //mainnet values
+    //<ConnectionProvider endpoint={MAINNET_ENDPOINT}>
+    //splToken={MAINNET_USDC_MINT}
+    //symbol="USDC"
+    //icon={<USDCIcon />}
+    //decimals={6}
+    //minDecimals={2}
+    
+    return (
+        <ThemeProvider>
+            <FullscreenProvider>
+                {recipient && label ? (
+                    <ConnectionProvider endpoint={DEVNET_ENDPOINT}>
+                        <WalletProvider wallets={wallets} autoConnect={connectWallet}>
+                            <WalletModalProvider>
+                                <ConfigProvider                                   
+                                    recipient={recipient}
+                                    label={label}
+                                    symbol="SOL"
+                                    icon={<SOLIcon />}
+                                    decimals={9}
+                                    minDecimals={1}
+                                    requiredConfirmations={9}
+                                    connectWallet={connectWallet}
+                                >
+                                    <TransactionsProvider>
+                                        <PaymentProvider>
+                                            <Outlet />
+                                        </PaymentProvider>
+                                    </TransactionsProvider>
+                                </ConfigProvider>
+                            </WalletModalProvider>
+                        </WalletProvider>
+                    </ConnectionProvider>
+                ) : (
+                    <div className={css.logo}>
+                        <SolanaPayLogo width={240} height={88} />
+                    </div>
+                )}
+            </FullscreenProvider>
+        </ThemeProvider>
+    );
+};
